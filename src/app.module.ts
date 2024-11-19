95413ef41f533975361d75c477d6e5c4f882267b
Jehyun Jung <congmul@hotmail.com>
2024-11-19 10:47:14 -0800

    Set env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { envValidate } from './env.validation';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/recreational-sports-league-mgmt'),
+    ConfigModule.forRoot({
+      isGlobal: true, // Make ConfigModule globally available
+      // If a variable is found in multiple files, the first one takes precedence.
+      envFilePath: ['.env.local', '.env'],
+      validate: envValidate
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'), // Access the MONGODB_URI variable
+      }),
+      inject: [ConfigService],
+    }),    
     UserModule
   ],
   controllers: [AppController],
