5871195b486ee78c6e09194f9d55990e1ed796eb
Jehyun Jung <congmul@hotmail.com>
2024-11-19 11:13:12 -0800

    Health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckService, MongooseHealthIndicator, HealthCheck } from '@nestjs/terminus';
 
+import { version } from '../../package.json';
+
 @Controller('health')
 export class HealthController {
     constructor(
@@ -10,10 +12,15 @@ export class HealthController {
 
     @Get()
     @HealthCheck()
-    check() {
-        return this.health.check([
+    async check() {
+        const healthCheckResult = await this.health.check([
             // Mongoose check to ensure the MongoDB connection is healthy
             () => this.mongoose.pingCheck('mongoose'),
         ])
+        return {
+            ...healthCheckResult,
+            msg: "Recreational sports league mgmt core service is healthy and ready to handle your requests.",
+            app_version: version
+        };
     }
 }
